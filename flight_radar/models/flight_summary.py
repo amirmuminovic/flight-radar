8f2f3d2c34ccb2e3282f85677443ad960005c9e0
Amir Muminović <37328370+amirmuminovic@users.noreply.github.com>
2025-10-23 01:35:51 +0200

    fix it

--- flight_radar/models/flight_summary.py
+++ flight_radar/models/flight_summary.py
@@ -78,9 +78,7 @@ class FlightSummaryBaseRequest(BaseModel):
             raise ValueError('Either flight_datetime_from and flight_datetime_to or flight_ids must be provided')
 
         field_names = list(FlightSummaryBaseRequest.model_fields.keys())
-        query_field_names = [
-            fn for fn in field_names if fn not in ['flight_datetime_from', 'flight_datetime_to']
-        ]
+        query_field_names = [fn for fn in field_names if fn not in ['flight_datetime_from', 'flight_datetime_to']]
         if all(getattr(self, field_name) is None for field_name in query_field_names):
             raise ValueError('At least one filter parameter must be provided')
 
